74d5bc097fd0d37bec0f391fddce8db5a15142b7
SteveViss <svissault01@gmail.com>
2025-12-03 12:23:41 -0500

    Simplify species_info.csv creation with barqueReport::process_species_info

--- inst/extdata/methodology.md
+++ inst/extdata/methodology.md
@@ -1,4 +1,5 @@
-This application displays species detected through environmental DNA (eDNA) metabarcoding analysis of water samples collected at **13 sampling stations** in October 2025. No replicates have been considered in laboratory.
+This application displays species detected through environmental DNA (eDNA) metabarcoding analysis of water samples collected at **46 sampling stations** in July 2024 and September 2025. No replicates have been considered in laboratory.
+
 
 eDNA extraction from samples and metagenomic analysis have been performed at the Laboratory of Valerie Langlois by Fidji Sandre (Ph.D.) and Julie Couillard (Ph.D.). Bioinformatics analysis was conducted by Steve Vissault (M.Sc).
 
@@ -15,5 +16,5 @@ The analysis uses the **12S ribosomal RNA (12S rRNA)** mitochondrial gene as a b
 
 ##### Sample and data Processing
 
-DNA is extracted from filtered water samples, followed by PCR amplification of the 12S rRNA gene region. The amplified DNA undergoes high-throughput sequencing with Illumina technology. Bioinformatics analysis is performed using **BARQUE** (Barcoding and Automated Recognition of QUality in Environmental DNA), and taxonomic assignment is conducted using the BARQUE internal reference databases pertaining 12S rRNA reference sequences for 2829 species.
+DNA is extracted from filtered water samples, followed by PCR amplification of the 12S rRNA gene region. The amplified DNA undergoes high-throughput sequencing with Illumina technology. Bioinformatics analysis is performed using BARQUE (Barcoding and Automated Recognition of QUality in Environmental DNA), and taxonomic assignment is conducted using the BARQUE internal reference databases pertaining 12S rRNA reference sequences for 2829 species.
 
--- scripts/enrich_species.R
+++ scripts/enrich_species.R
@@ -25,73 +25,37 @@ require(rvest)
 
 getwikipic <- function(titles, res = 220, savedest = NA) {
     lapply(titles, function(ttl, ...) {
-        d <- page_info("en", "wikipedia", page = ttl, clean_response = T)
-        url <- d[[1]]$fullurl
-        wikipage <- session(url)
-        imginfo <- wikipage %>% html_nodes("tr:nth-child(2) img")
-        img.url <- imginfo[1] %>% html_attr("src")
-        img.url <- paste0("https:", img.url)
+        tryCatch({
+            d <- page_info("en", "wikipedia", page = ttl, clean_response = T)
+            url <- d[[1]]$fullurl
+            wikipage <- session(url)
+            imginfo <- wikipage %>% html_nodes("tr:nth-child(2) img")
+            img.url <- imginfo[1] %>% html_attr("src")
+            img.url <- paste0("https:", img.url)
 
-        if (is.na(savedest)) {
-            savefilename <- paste0(ttl, ".jpg")
-        } else {
-            savefilename <- paste0(savedest, ttl, ".jpg")
-        }
+            if (is.na(savedest)) {
+                savefilename <- paste0(ttl, ".jpg")
+            } else {
+                savefilename <- paste0(savedest, ttl, ".jpg")
+            }
 
-        if (res != 220) {
-            img.url <- gsub(220, res, img.url)
-        }
+            if (res != 220) {
+                img.url <- gsub(220, res, img.url)
+            }
 
-        download.file(img.url, stringr::str_replace_all(savefilename," ", "_"))
-        return(paste0("orig.file: ", basename(img.url))) # tell user original filename (or error)
+            download.file(img.url, stringr::str_replace_all(savefilename," ", "_"))
+            return(paste0("orig.file: ", basename(img.url)))
+        }, error = function(e) {
+            return(paste0("Error for ", ttl, ": ", e$message))
+        })
     }, res, savedest)
 } 
 
-getwikipic(titles = sps, res = 400, savedest = "data/img/")
+getwikipic(titles = sps, res = 400, savedest = "inst/extdata/img/")
 
-species_consolidated <- sps_info |>
-    dplyr::group_by(
-        species,
-        English,
-        French,
-        gbif_url,
-        col_link,
-        itis_url
-    ) |>
-    dplyr::summarise(
-        Native_Quebec = any(native == TRUE & !is.na(Quebec)),
-        Exotic_Quebec = any(exotic == TRUE & !is.na(Quebec)),
-        .groups = "drop"
-    ) |>
-    dplyr::mutate(
-        img = ifelse(file.exists(paste0("inst/extdata/img/", stringr::str_replace_all(species, " ", "_"), ".jpg")), paste0("img/", stringr::str_replace_all(species, " ", "_"), ".jpg"), NA)
-    ) 
+species_info <- barqueReport::process_species_info(sps)
+species_info <- species_info |> dplyr::mutate(
+            img = ifelse(file.exists(paste0("inst/extdata/img/", stringr::str_replace_all(species, " ", "_"), ".jpg")), paste0("img/", stringr::str_replace_all(species, " ", "_"), ".jpg"), NA)
+        )
 
-status_qc <- read.csv("inst/extdata/QC_especes_en_peril.csv")
-status_ca <- read.csv("inst/extdata/CA_especes_en_peril.csv")
-
-# Join conservation status data
-# Remove duplicates from conservation status files by keeping the first occurrence
-status_ca_unique <- status_ca |>
-  dplyr::select(species = Nom.scientifique, status_ca = Statut.à.l.annexe.1) |>
-  dplyr::distinct(species, .keep_all = TRUE)
-
-status_qc_unique <- status_qc |>
-  dplyr::select(species = Nom_scientifique, status_qc = STATUT_LEMV) |>
-  dplyr::distinct(species, .keep_all = TRUE)
-
-species_consolidated |>
-    dplyr::left_join(
-        status_ca_unique,
-        by = c("species")
-    ) |>
-    dplyr::left_join(
-        status_qc_unique,
-        by = c("species")
-    ) |>
-    dplyr::mutate(
-        status_ca = ifelse(status_ca == "Aucun statut", NA, status_ca),
-        status_qc = ifelse(status_qc == "Non suivie", NA, status_qc)
-    ) |> write.csv("inst/extdata/species_info.csv", row.names = FALSE)
-
-
+write.csv(species_info, "inst/extdata/species_info.csv", row.names = FALSE)
